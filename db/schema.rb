0ec3d36dece7110f5dbe74683e984a791dbf7e57
Álvaro Molina <abusmolina@gmail.com>
2019-08-19 21:47:05 -0400

    Add faker

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_010344) do
+ActiveRecord::Schema.define(version: 2019_08_20_014218) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2019_08_20_010344) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "questions", force: :cascade do |t|
